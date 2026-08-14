eefceca0adaddde97b5a5b059110c036c2d01e34
lingwangshu018 <lingwangshu018@gmail.com>
2026-08-14 10:08:24 +0800

    chore: clean up video call archive card

--- app/src/main/java/me/rerere/rikkahub/ui/components/message/VideoCallArchiveCard.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/components/message/VideoCallArchiveCard.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import me.rerere.rikkahub.ui.pages.voice.VideoCallArchiveEntry
-import me.rerere.rikkahub.ui.pages.voice.VideoCallArchiveRuntime
 import me.rerere.rikkahub.ui.pages.voice.VideoCallArchiveStore
 import kotlin.math.max
 
